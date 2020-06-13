c28100457c2c3be1cc0ed3b4420813e0051458d5
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-06-13 23:02:57 +0530

    Login fixed

--- Samriddhi/apiblog/apiblog/settings.py
+++ Samriddhi/apiblog/apiblog/settings.py
@@ -84,7 +84,7 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'apiblog',
@@ -92,14 +92,14 @@ REST_FRAMEWORK = {
         'PASSWORD': '9457047004',
         'HOST': 'localhost'
     }
-}'''
-
+}
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}'''
 
 
 # Password validation
